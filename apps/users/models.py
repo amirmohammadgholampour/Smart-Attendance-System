68857b0e0f566a74c25576c1bdedee9ef412d6ce
amirmohammadgholampour <amirmohammadghlmpr@gmail.com>
2026-05-10 19:32:00 +0330

    update the models

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Custom User model inheriting from Django's AbstractUser.
+    All required fields (first_name, last_name, username, password, email) are inherited.
+    Email is set to unique to avoid duplicate emails.
+    """
+    email = models.EmailField(unique=True, blank=False)
+    
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        related_name='custom_user_groups',
+        related_query_name='custom_user',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name='custom_user_permissions',
+        related_query_name='custom_user',
+    )
+
+    class Meta:
+        db_table = 'users_user'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.get_full_name() or self.username
